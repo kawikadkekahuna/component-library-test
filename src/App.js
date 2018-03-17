c476d75ce0e0170670a36ce3a251fb591d5e7608
Kawika Kekahuna <kawikakekahuna@gmail.com>
2018-03-17 10:47:21 -1000

    init test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { PrimaryButton as Button } from './Components/Button';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Button/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const PrimaryButton = () => {
+  return (
+    <button>Hello World</button>
+  )
+}
